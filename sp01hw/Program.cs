bc97ceb5d286705d5f273ac7e70f548bea5e0745
dekhtiarenkoOleh <sbirok1232@gmail.com>
2024-08-22 23:04:34 -0400

    Add project files.

---
+++ sp01hw/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sp01hw
+{
+    internal class Program
+    {
+        static class DLLexample
+        {
+            [DllImport("User32.dll")]
+            public static extern int GetSystemMetrics(int inta);
+            public const int SM_CXSCREEN = 0;
+            public const int SM_CYSCREEN = 1;
+        }
+        static void Main(string[] args)
+        {
+            int screenWidth = DLLexample.GetSystemMetrics(DLLexample.SM_CXSCREEN);
+            int screenHeight = DLLexample.GetSystemMetrics(DLLexample.SM_CYSCREEN);
+            Console.WriteLine("Разрешение {0}x{1}", screenWidth, screenHeight);
+            Console.Write("Enter your PPI -> ");
+            double PPI = double.Parse(Console.ReadLine());
+            Console.WriteLine($"You have : {Math.Round((double)Math.Sqrt(Math.Pow(screenHeight, 2) + Math.Pow(screenWidth, 2))/PPI,2)} inches.");
+            Console.ReadKey();
+        }
+    }
+}
